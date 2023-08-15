bf0c8ad6151e1fbdac7577300ba317544b189fa2
Thomas Burton <tburton5572@gmail.com>
2023-08-15 15:25:27 +0100

    mass matrix with simple interface

--- src/DAESolve.jl
+++ src/DAESolve.jl
@@ -4,28 +4,36 @@ using LinearAlgebra
 
 using NKRoots
 
-export solvedae!
+export MassMatrix, solvedae!
+
+include("massmatrix.jl")
 
 # update the state given the evolution and constraints
+# TODO: IN-PLACE!!!
 function _update!(q_new, q_old, F, G, Δτ::Real, M::AbstractMatrix, rootopts::Options)
     # define an objective function from F and G
     function objective!(out, q_new)
-        @views out[1:size(M, 1)] .= M*(q_new .- q_old) .+ Δτ.*F(q_new)
+        @views out[1:size(M, 1)] .= LinearAlgebra.mul!(similar(q_old), M, q_new .- q_old) .+ Δτ.*F(q_new)
         @views out[(size(M, 1) + 1):end] .= G(q_new)
 
         return out
     end
 
     # perform the root finding with nkroot!
-    # FIXME: extend NKRoots and GMRES interfaces for proper in-place computations all the way
     nkroot!(objective!, q_new, rootopts)
 
     return q_new
 end
 
 # evolve the system until a condition is met
-function solvedae!(q, q_init, F, G, Δτ::Real, M::AbstractMatrix; stopcrit=nothing, rootopts::Options=Options(), verbose::Bool=false)
+function solvedae!(q, q_init, F, G, Δτ::Real, M::AbstractMatrix=I; stopcrit=nothing, rootopts::Options=Options(), verbose::Bool=false)
     # initialise variable for old state
+
+    # determine mass matrix if necessary
+    if M isa UniformScaling
+        M = MassMatrix(length(F(q_init)))
+    end
+
     # if mass matrix isn't provided construct our own
     # loop over time steps
         # check if stopping criteria has been met
---
+++ src/massmatrix.jl
@@ -0,0 +1,18 @@
+# This file defines a simple interface for a mass matrix to ensure efficient
+# use of memory.
+
+struct MassMatrix <: AbstractMatrix{Int64}
+    D::Vector{Int64}
+end
+
+# default constructor for simple case where first N elements are being evolved
+MassMatrix(N::Int64) = MassMatrix([n for n in 1:N])
+
+# efficiently pick the indices of the state that are evolving
+function LinearAlgebra.mul!(p::V, M::MassMatrix, q::V) where {V}
+    for i in eachindex(M.D)
+        p[i] = q[D[i]]
+    end
+
+    return p
+end
