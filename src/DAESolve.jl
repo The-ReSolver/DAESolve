c83199edc4f68e15b919b54aafe2deabb92cdb73
Thomas Burton <tburton5572@gmail.com>
2023-08-15 13:56:04 +0100

    basic update method for DAE time step

--- src/DAESolve.jl
+++ src/DAESolve.jl
@@ -1,14 +1,19 @@
 module DAESolve
 
+using LinearAlgebra
+
 using NKRoots
 
 export solvedae!
 
 # update the state given the evolution and constraints
-function _update!(q_new, q_old, F!, G!, Δτ, rootopts)
-    # define an objective function from F! and G!
+function _update!(q_new, q_old, F, G, Δτ, M, rootopts)
+    # define an objective function from F and G
     function objective!(out, q_new)
-        
+        @views out[1:size(M, 1)] .= M*(q_new .- q_old) .+ Δτ.*F(q_new)
+        @views out[(size(M, 1) + 1):end] .= G(q_new)
+
+        return out
     end
 
     # perform the root finding with nkroot!
@@ -19,8 +24,9 @@ function _update!(q_new, q_old, F!, G!, Δτ, rootopts)
 end
 
 # evolve the system until a condition is met
-function solvedae!(q, q_init, F!, G!, Δτ; stopcrit=nothing, rootopts=Options(), verbose::Bool=false)
+function solvedae!(q, q_init, F, G, Δτ, M::AbstractMatrix; stopcrit=nothing, rootopts=Options(), verbose::Bool=false)
     # initialise variable for old state
+    # if mass matrix isn't provided construct our own
     # loop over time steps
         # check if stopping criteria has been met
         # update the state at each time step
@@ -28,6 +34,6 @@ function solvedae!(q, q_init, F!, G!, Δτ; stopcrit=nothing, rootopts=Options()
         # do some printing
     # return the solution
 end
-solve(q_init, F!, G!, Δτ; stopcrit=nothing, rootopts=Options(), verbose::Bool=false) = solve!(copy(q_init), q_init, F!, G!, Δτ, stopcrit=stopcrit, rootopts=rootopts, verbose=verbose)
+solve(q_init, F, G, Δτ; stopcrit=nothing, rootopts=Options(), verbose::Bool=false) = solve!(copy(q_init), q_init, F, G, Δτ, stopcrit=stopcrit, rootopts=rootopts, verbose=verbose)
 
 end
